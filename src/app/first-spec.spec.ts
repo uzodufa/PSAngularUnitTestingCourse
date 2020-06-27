e4800d135bc79dfb675cf3ac7397634fa09be7bb
Ebere Uzodufa <uzodufa@systemspecs.com.ng>
2020-06-27 11:46:25 +0100

    Add descriptions

--- src/app/first-spec.spec.ts
+++ src/app/first-spec.spec.ts
@@ -1,18 +1,19 @@
 describe('my first test', () => {
-  let sut;
+  let sut; //System under test
 
+  //beforeEach helps make sure that the system (in this case sut) is restarted every time.
   beforeEach(() => {
     sut = {}
   })
 
   it('should be true if true', () => {
-    // arrange
+    // arrange A
     sut.a = false;
 
     // act
     sut.a =  true;
 
-    // assert
+    // assert AAA
     expect(sut.a).toBe(true);
   });
 });
